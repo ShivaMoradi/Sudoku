8b6e640147e6235e9f289083ffb7a0c3e8117e99
ShivaMoradi <shiva.moradi1@gmail.com>
2024-02-02 21:39:03 +0100

    added function solve to solve the puzzle automaticly

--- puzzle.py
+++ puzzle.py
@@ -1,5 +1,6 @@
 # importerar NumPy
 import numpy as np
+from pandas.core.interchange import column
 
 # Skapa en np grid/matrix med given siffror
 given = [
@@ -14,13 +15,25 @@ given = [
     [9, 3, 0, 0, 0, 0, 0, 2, 1]
 ]
 ngiven = np.matrix(given)
-# skriva ut matrixen
-print(ngiven)
 
+
+# skriva ut matrixen
 
 def board(row, column, number):  # definiera funktion till pussel
     global ngiven
-    # finns siffran inne i raden?
+    print(f"Guesse a number: ")  # En användare kan gissa en siffran mellan 1-9
+    try:
+        number = int(number)
+        if 1 <= number < 10:
+            print(f"You guessed: {number}")
+        else:
+            print(f"The number should be between 1 and 9")
+            return False
+            # finns siffran inne i raden?
+    except ValueError:
+        print("Please")
+        return False
+
     for i in range(0, 9):
         if ngiven[row, i] == number:
             print(False)
@@ -38,4 +51,38 @@ def board(row, column, number):  # definiera funktion till pussel
                 print(False)
     print(True)
 
-board(1,0, 7)
+
+def prettify():
+    print("Output:  ")
+    global given
+    j = 0
+    for row in given:
+        i = 0
+        j += 1
+        for item in row:
+            i += 1
+            print(f" {item} ", end='')
+            if i % 3 == 0 and i < 9:
+                print(" | ", end='')
+        print()
+        if j % 3 == 0 and j < 9:
+            print(' - ' * 11)
+
+
+def solve():
+    global ngiven
+    for row in range(9):
+        for column in range(9):
+            if given[row][column] == 0:
+                for number in range(1, 10):
+                    if board(row, column, number):
+                        given[row][column] = number
+                        if solve():
+                            return True
+                        given[row][column] = 0
+                return False
+    prettify()
+
+
+prettify()
+solve()
